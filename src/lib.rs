e0f880dcb4704ae091f3610a2550b86ac2ecd9cd
Lut99 <mullertim99@gmail.com>
2023-04-20 19:07:34 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+//  LIB.rs
+//    by Lut99
+// 
+//  Created:
+//    20 Apr 2023, 19:07:02
+//  Last edited:
+//    20 Apr 2023, 19:07:08
+//  Auto updated?
+//    Yes
+// 
+//  Description:
+//!   A niche little crate that simplifies creating large hardcoded
+//!   directory structures.
+// 
+
+
